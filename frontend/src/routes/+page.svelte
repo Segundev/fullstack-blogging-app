5ee86eb461787d943d1e4e52e18d5004f4094820
Segundev <jayesegun03@gmail.com>
2024-08-28 13:11:28 +0100

    fix: fixed import assets dependency and @apply directive in +page.svelte style

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,7 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h2>Trying Stuff Out</h2>
 
 <style lang="postcss">
 	:global(html) {
-		background-color: theme(colors.gray.100);
+		@apply bg-gray-100;
 	}
 </style>
